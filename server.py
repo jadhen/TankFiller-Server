505df0218d96141fdd0f862e8085f0372bee3b2c
jadhen <jadwhen474@student.polsl.pl>
2017-01-04 22:41:39 +0100

    add fillup

--- server.py
+++ server.py
@@ -4,10 +4,11 @@ from flask import request
 from flask_pymongo import PyMongo
 
 # -*- coding: utf-8 -*-
-from datetime import timedelta
+from datetime import timedelta, datetime
 from flask import make_response, request, current_app
 from functools import update_wrapper
 from bson.objectid import ObjectId
+
 
 def crossdomain(origin=None, methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
@@ -131,3 +132,17 @@ def add_new_car():
     car = mongo.db.cars
     result = car.insert_one({'manufacturer' : man, 'model': mod, 'prod_year' : prod, 'mileage' : mil, 'userid' : ObjectId(userid)})
     return jsonify({'acknowledged' : result.acknowledged, 'inserted_id' : str(result.inserted_id)})
+
+@app.route('/car/fillup/new',methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
+def add_new_fillup():
+    km = request.form['driven_km']
+    pr = request.form['price']
+    lit = request.form['liters']
+    date = request.form['date']
+    carid = request.form['carid']
+    full = request.form['fulltank']
+    car = mongo.db.cars
+    car.update_one({'_id' : ObjectId(carid) }, {'$inc': {'mileage': float(km)}})
+    result = car.update_one({'_id' : ObjectId(carid) }, { '$push': {  'fillups': { '$each': [ { 'date' :datetime.strptime(date, "%Y-%m-%d"), 'liters' : lit, 'driven_km' : km, 'per_liter' : pr , 'fullTank' : full }],'$sort': { 'date': 1 }}}})
+    return jsonify({'acknowledged' : result.acknowledged, 'modified_count' : result.modified_count})
