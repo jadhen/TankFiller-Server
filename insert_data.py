9ffe03c35ed98f4d03ba947a9c554d074fde80f8
jadhen <jadwhen474@student.polsl.pl>
2017-01-04 22:42:10 +0100

    missing attribute driven_km added

--- insert_data.py
+++ insert_data.py
@@ -1,6 +1,7 @@
 from  pymongo import MongoClient, errors
 from pymongo.errors import CollectionInvalid
 import datetime
+from bson.objectid import ObjectId
 client = MongoClient()
 client.drop_database('TankFiller')
 db = client.TankFiller
@@ -20,16 +21,16 @@ models.insert({"manufacturer_name": "Renault", "models": [{"name": 'Clio', "vers
 
 cars = db.cars
 cars.insert({"userid": id, "manufacturer": "Renault","model" : "Clio 1", "prod_year": 2001,  "mileage" : 100789, "fillups" :
-    [{"date" : datetime.datetime(2016, 06, 01), 'fullTank' : True, "liters":'25.6', "per_liter" : "4.67" },
-    {"date" : datetime.datetime(2016, 06, 28), 'fullTank' : True, "liters":'18.6', "per_liter" : "4.78" },
-    {"date" : datetime.datetime(2016, 07, 10), 'fullTank' : True, "liters":'21.6', "per_liter" : "4.44" },
-    {"date" : datetime.datetime(2016, 07, 31), 'fullTank' : True, "liters":'22.1', "per_liter" : "4.90" },
-    {"date" : datetime.datetime(2016, 10, 19), 'fullTank' : False, "liters":'10.2', "per_liter" : "4.54" },
-    {"date" : datetime.datetime(2016, 10, 20), 'fullTank' : True, "liters":'23.2', "per_liter" : "4.70" },
-    {"date" : datetime.datetime(2016, 10, 25), 'fullTank' : True, "liters":'19.6', "per_liter" : "4.44" },
-    {"date" : datetime.datetime(2016, 11, 01), 'fullTank' : False, "liters":'16.4', "per_liter" : "4.23" },
-    {"date" : datetime.datetime(2016, 11, 20), 'fullTank' : True, "liters":'20.2', "per_liter" : "4.12" },]})
-print(models.find({}))
+    [{"date" : datetime.datetime(2016, 06, 01), 'fullTank' : True, "liters":'25.6', "per_liter" : "4.67", "driven_km" :127 },
+    {"date" : datetime.datetime(2016, 06, 28), 'fullTank' : True, "liters":'18.6', "per_liter" : "4.78" , "driven_km" :134},
+    {"date" : datetime.datetime(2016, 07, 10), 'fullTank' : True, "liters":'21.6', "per_liter" : "4.44", "driven_km" :155 },
+    {"date" : datetime.datetime(2016, 07, 31), 'fullTank' : True, "liters":'22.1', "per_liter" : "4.90" , "driven_km" :189},
+    {"date" : datetime.datetime(2016, 8, 19), 'fullTank' : False, "liters":'10.2', "per_liter" : "4.54" , "driven_km" :133},
+    {"date" : datetime.datetime(2016, 9, 10), 'fullTank' : True, "liters":'23.2', "per_liter" : "4.70" , "driven_km" :190},
+    {"date" : datetime.datetime(2016, 10, 01), 'fullTank' : True, "liters":'19.6', "per_liter" : "4.44" , "driven_km" :202},
+    {"date" : datetime.datetime(2016, 10, 22), 'fullTank' : False, "liters":'16.4', "per_liter" : "4.23" , "driven_km" :177},
+    {"date" : datetime.datetime(2016, 11, 7), 'fullTank' : True, "liters":'20.2', "per_liter" : "4.12" , "driven_km" :188},]})
+#print(models.find({}))
 
 for c in cars.find({}):
     print(c)
